794e040fb016c059b57e4aa65341fa37bc4b99ee
caio-chcn <caioh.mec@gmail.com>
2022-01-27 19:50:42 -0300

    quarta atividade

---
+++ script.js
@@ -0,0 +1,21 @@
+//1 - crie uma função que exiba uma mensagem no console
+function quartaatividade(){
+    console.log("apanhando, mas fazendo kkkk")
+}
+quartaatividade()
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function mostrarnome(){
+    console.log("Caio")
+}
+mostrarnome()
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+function sobrecaio(){
+console.log("caio","30","rap")
+}
+sobrecaio()
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function filmemusica(){
+    console.log("Django Livre","Dela Soul- Stakes is High")
+}
+    filmemusica()
